26a73fa6e513a687e1200190ca70344d6b1529c4
Frankenstine Joe <Frankenstine@DESKTOP-KCNC5C4>
2017-05-15 13:07:11 +0500

    All Files Upload

---
+++ All permutations ForString/CSharpCrawlerPermute.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace All_permutations_ForString
+{
+    class CSharpCrawlerPermute
+    {
+    }
+
+    class Permute
+    {
+        public static List<string> AllCombos { get; set; }
+
+        private void swap(ref char a, ref char b)
+        {
+            if (a == b) return;
+
+            a ^= b;
+
+            b ^= a;
+
+            a ^= b;
+        }
+
+        public void setper(char[] list)
+        {
+            int x = list.Length - 1;
+
+            go(list, 0, x);
+        }
+
+        private void go(char[] list, int k, int m)
+        {
+            int i;
+
+            if (k == m)
+
+            {
+
+                Console.Write(list);
+
+                Console.WriteLine(" ");
+
+            }
+            else
+                for (i = k; i <= m; i++)
+                {
+
+                    swap(ref list[k], ref list[i]);
+
+                    go(list, k + 1, m);
+
+                    swap(ref list[k], ref list[i]);
+                }
+        }
+
+    }
+
+
+}
---
+++ All permutations ForString/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace All_permutations_ForString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region C# crawler Website Code
+
+            var x = Stopwatch.StartNew();
+
+            Permute p = new Permute();
+
+            string c = "abcdefghi";
+
+            char[] c2 = c.ToCharArray();
+
+            /*calling the permute*/
+
+            p.setper(c2);
+
+            x.Stop();
+
+            Console.WriteLine(x.Elapsed);
+
+            Console.ReadKey();
+
+            #endregion
+
+            #region Frank Code
+
+            //Console.Write("Enter Digits : ");
+
+            //int Digits = int.Parse(Console.ReadLine());
+
+            //int End = 'a' + Digits;
+
+            //char EndChar = (char)End;
+
+            //Console.WriteLine(EndChar.ToString());
+
+            //Console.WriteLine(End);
+
+
+            //List<string> Output = new List<string>();
+
+            //for (int i = 'a'; i < EndChar; i++)
+            //{
+            //    string temp=
+            //    Output.Add("");
+            //}
+
+            int sum = 1;
+            for (int i = 14; i > 0; i--)
+            {
+                sum = sum * i;
+            }
+
+            Console.WriteLine(sum);
+            Console.ReadKey();
+            #endregion
+        }
+
+
+
+
+    }
+}
